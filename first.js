23f5dfdc850806432a48fc40f51f55f833f81268
ronokjahan <ronok554429@gmail.com>
2023-02-19 12:18:25 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,6 @@
+// set number 1 
+document.getElementById('btn-submit').addEventListener('click',function(){
+  const emailField = document.getElementById('user-email');
+  const email = emailField.value;
+ console.log(test);
+})
